8691b6f21030460c344b2b1d801c5d36ec7cecdc
LachlanMarnham <lachlan.marnham@gmail.com>
2018-06-02 16:09:27 +0100

    Changes to base db tests.

--- tests/backend_tests/test_database.py
+++ tests/backend_tests/test_database.py
@@ -2,30 +2,31 @@ import os
 import pytest
 
 import context
-from backend.database.db import BaseDBConnection, PATH_TO_DB
+from backend.database.db import BaseDBConnection, SCHEMA
 from backend.exceptions import DatabaseError
 
+PATH_TO_TEST_DB = 'db.db'
 
 class DBContextManager(object):
     def __init__(self, delete_after_test=True):
         self.delete_after_test = delete_after_test
 
     def __enter__(self):
-        if os.path.exists(PATH_TO_DB):
-            os.remove(PATH_TO_DB)
+        if os.path.exists(PATH_TO_TEST_DB):
+            os.remove(PATH_TO_TEST_DB)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         try:
             if self.delete_after_test:
-                os.remove(PATH_TO_DB)
+                os.remove(PATH_TO_TEST_DB)
         except FileNotFoundError:
-            raise DatabaseError("DB deletion failed in post-test. {} does not exist.".format(PATH_TO_DB))
+            raise DatabaseError("DB deletion failed in post-test. {} does not exist.".format(PATH_TO_TEST_DB))
 
 
 @pytest.fixture
 def base_db():
     with DBContextManager():
-        yield BaseDBConnection
+        yield BaseDBConnection(path_to_db=PATH_TO_TEST_DB, schema=SCHEMA)
 
 
 class TestBaseDB(object):

bc0f3270f79c5e267fdeabead5f857483734a9a5
LachlanMarnham <lachlan.marnham@gmail.com>
2018-09-25 23:50:35 +0100

    Added type hinting to collection types

--- backend/entities/collection_types.py
+++ backend/entities/collection_types.py
@@ -7,18 +7,18 @@ class BaseCollection(list):
         super().__init__(iterable if iterable else [])
         self.next_up_index = next_up_index
 
-    def all_played(self):
+    def all_played(self) -> bool:
         """
-        Work out if it is time to schedule the items again
+        Returns true if all items have been played since the collection was last shuffled, else False
         """
         played_gen = (item.played for item in self)
         return all(played_gen)
 
-    def mark_all_items_as_unplayed(self):
+    def mark_all_items_as_unplayed(self) -> None:
         for item in self:
             item.mark_as_unplayed()
 
-    def reschedule_all(self):
+    def reschedule_all(self) -> None:
         self.mark_all_items_as_unplayed()
         self.shuffle()
         self.next_up_index = 0

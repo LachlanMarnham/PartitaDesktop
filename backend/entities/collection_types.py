0c38d1ad9446a4f077ee1f869d748f75df54559d
LachlanMarnham <lachlan.marnham@gmail.com>
2018-09-25 22:06:51 +0100

    Added some scheduling functionality to BaseCollection

--- backend/entities/collection_types.py
+++ backend/entities/collection_types.py
@@ -3,8 +3,30 @@ from random import shuffle
 
 class BaseCollection(list):
 
-    def __init__(self, iterable=None):
+    def __init__(self, iterable=None, next_up_index=0):
         super().__init__(iterable if iterable else [])
+        self.next_up_index = next_up_index
+
+    def all_played(self):
+        """
+        Work out if it is time to schedule the items again
+        """
+        played_gen = (item.played for item in self)
+        return all(played_gen)
+
+    def mark_all_items_as_unplayed(self):
+        for item in self:
+            item.mark_as_unplayed()
+
+    def reschedule_all(self):
+        self.mark_all_items_as_unplayed()
+        self.shuffle()
+        self.next_up_index = 0
+
+    def get_next_item(self):
+        next_item = self.__getitem__(self.next_up_index)
+        self.next_up_index += 1
+        return next_item
 
     @property
     def length(self):
